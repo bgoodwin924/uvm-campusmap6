2156f2ef6d4a2b00e446224605f4dda069505944
David Duong <david.duong03@gmail.com>
2017-04-01 16:48:37 -0400

    css changes and index

--- nav.php
+++ nav.php
@@ -2,31 +2,25 @@
 <nav>
     <ul class='nav'>
         <?php
-        print '<li class="';
         if ($path_parts['filename'] == "index") {
-            print ' activePage ';
+            print '<li class="activePage">Home</li>';
+        } else {
+            print '<li><a href="index.php">Home</a></li>';
         }
-        print '">';
-        print '<a href="index.php">Home</a>';
-        print '</li>';
-        
+      
         //Link to Waterman building
-        print '<li class="';
-        if ($path_parts['filename'] == "waterman") {
-            print ' activePage ';
+      if ($path_parts['filename'] == "waterman") {
+            print '<li class="activePage">Waterman</li>';
+        } else {
+            print '<li><a href="waterman.php">Waterman</a></li>';
         }
-        print '">';
-        print '<a href="waterman.php">Waterman</a>';
-        print '</li>';
         
         //Link to Davis Center
-         print '<li class="';
-        if ($path_parts['filename'] == "daviscenter") {
-            print ' activePage ';
+       if ($path_parts['filename'] == "daviscenter") {
+            print '<li class="activePage">Davis Center</li>';
+        } else {
+            print '<li><a href="daviscenter.php">Davis Center</a></li>';
         }
-        print '">';
-        print '<a href="daviscenter.php">Davis Center</a>';
-        print '</li>';
         ?>
     </ul>
 </nav>
